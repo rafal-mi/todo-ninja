eebaeb74799be2c8a30dcfce76034da8b561292e
Rafal Mi <rafal.m4@gmail.com>
2019-09-11 14:50:37 +0200

    Almost completed #4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>Home page</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut fugit laborum eveniet! Iste repellat, corporis minima rem possimus magnam ipsam ipsa, nobis delectus fugit sunt, voluptates aliquam obcaecati! Omnis, asperiores.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat ad fugit animi nihil. Architecto inventore, alias, iure reiciendis accusamus quia, sequi sit libero ullam eum nemo sed amet labore facilis?</p>
+    <h1 class="display-4">Massive Display</h1>
+
+    <h4 class="display-1">Smaller display</h4>
+
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a subheading</p>
+    <p class="caption">This is a caption</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
 
 export default {
-  components: {
-    HelloWorld,
-  },
 };
 </script>
