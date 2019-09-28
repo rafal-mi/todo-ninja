a42486ecdf85f8d61a629103e6596498e05b20d2
Rafal Mi <rafal.m4@gmail.com>
2019-09-28 22:55:31 +0200

    #13 Grid

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,22 +3,30 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
-        <v-row wrap>
-          <v-col xs12 md6>
+        <v-row>
+          <v-col xs="12" sm="12" md="6">
             <v-btn outlined block class="">1</v-btn>
           </v-col>
-          <v-col xs4 md2>
+          <v-col xs="4" sm="2" md="2">
             <v-btn outlined block class="">2</v-btn>
           </v-col>
-          <v-col xs4 md2>
+          <v-col xs="4" sm="2" md="2">
             <v-btn outlined block class="">2</v-btn>
           </v-col>
-          <v-col xs4 md2>
+          <v-col xs="4" sm="2" md="2">
             <v-btn outlined block class="">2</v-btn>
           </v-col>
-
-
         </v-row>
+
+        <v-row justify="space-around">
+          <v-col xs="4" md="3">
+            <v-btn outlined block class="success">1</v-btn>
+          </v-col>
+          <v-col xs="4" md="3">
+            <v-btn outlined block class="success">1</v-btn>
+          </v-col>
+        </v-row>
+
     </v-container>
 
   </div>
